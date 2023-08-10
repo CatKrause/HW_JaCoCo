dc931c62bd52c00e7b041ec6d7d5e0ce46742858
Ekaterina Khritonenkova <Catybym@gmail.com>
2023-08-10 15:29:29 +0300

    total commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,41 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxOneIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] oneIncomes = {2};
+
+        long expected = 2;
+        long actual = service.findMax(oneIncomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNegativeIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] negativeIncomes = {-2, -3, -11, -25, -13};
+
+        long expected = -2;
+        long actual = service.findMax(negativeIncomes);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] maxIncome = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(maxIncome);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
